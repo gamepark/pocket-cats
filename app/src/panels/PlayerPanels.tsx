68e8efc23a857a554c827b6acf36e3be285fb9c2
Romain Fromi <romain.fromi@gmail.com>
2024-09-11 10:14:32 +0200

    Initial commit

---
+++ app/src/panels/PlayerPanels.tsx
@@ -0,0 +1,37 @@
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react'
+import { PlayerColor } from '@gamepark/game-template/PlayerColor'
+import { StyledPlayerPanel, usePlayers } from '@gamepark/react-game'
+import { FC } from 'react'
+import { createPortal } from 'react-dom'
+
+export const PlayerPanels: FC<any> = () => {
+  const players = usePlayers({ sortFromMe: true })
+  const root = document.getElementById('root')
+  if (!root) {
+    return null
+  }
+
+  return createPortal(
+    <>
+      {players.map((player, index) =>
+        <StyledPlayerPanel key={player.id} player={player} color={playerColorCode[player.id]} css={panelPosition(index)}/>
+      )}
+    </>,
+    root
+  )
+}
+const panelPosition = (index: number) => css`
+  position: absolute;
+  right: 1em;
+  top: ${8.5 + index * 16}em;
+  width: 28em;
+  height: 14em;
+`
+
+export const playerColorCode: Record<PlayerColor, string> = {
+  [PlayerColor.Red]: 'red',
+  [PlayerColor.Blue]: 'blue',
+  [PlayerColor.Green]: 'green',
+  [PlayerColor.Yellow]: 'yellow'
+}
